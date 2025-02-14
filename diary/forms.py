e6d67126492ae303a20d669d8e7556de775eea56
ks0727 <kouske51@icloud.com>
2025-02-14 14:55:10 +0900

    update settings and add email function

--- diary/forms.py
+++ diary/forms.py
@@ -1,5 +1,6 @@
+import os
 from django import forms
-
+from django.core.mail import EmailMessage
 class InquiryForm(forms.Form):
     name = forms.CharField(label='お名前',max_length=30)
     email = forms.EmailField(label='メールアドレス')
@@ -21,4 +22,21 @@ class InquiryForm(forms.Form):
         self.fields['message'].widget.attrs['class'] = 'form-control'
         self.fields['message'].widget.attrs['placeholder'] ='メッセージをここに入力してください'
         
+    def send_email(self):
+        name = self.cleaned_data['name']
+        email = self.cleaned_data['email']
+        title = self.cleaned_data['title']
+        message = self.cleaned_data['message']
         
+        subject = f'お問い合わせ{title}'
+        message = f'送信者: {name}\nメールアドレス: {email}\nメッセージ  {message}\n'
+        from_email = os.environ.get('FROM_EMAIL')
+        to_list = [
+            os.environ.get('FROM_EMAIL')
+        ]
+        cc_list = [
+            email
+        ]
+        
+        message = EmailMessage(subject=subject,body=message,from_email=from_email,to=to_list,cc=cc_list)
+        message.send()
---
+++ diary_app/settings_dev.py
@@ -0,0 +1,45 @@
+from .settings_common import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-cgzefg029oa@84@@*e6*8^tk9ik2i@7(39q$o87)$8pqx0bw8_'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+LOGGING = {
+    'version':1,
+    'disable_existing_loggers':False,
+    
+    'loggers':{
+        'django':{
+            'handlers':['console'],
+            'level':'INFO',
+        },
+        'diary':{
+            'handlers':['console'],
+            'level':'DEBUG',
+        },
+    },
+    'handlers':{
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+            'formatter':'dev',
+        },
+        
+    },
+    'formatters':{
+        'dev':{
+            'format':'\t'.join([
+                '%(asctime)s',
+                '[%(levelname)s]',
+                '%(pathname)s(Line:%(lineno)d)',
+                '%(message)s',
+            ])
+        }
+    },
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
